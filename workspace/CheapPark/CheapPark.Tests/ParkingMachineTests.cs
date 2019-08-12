af8cace7b722a88894a6a0912809d0f291c3c89d
Andreas Fyrstelin Kristiansen <afkpost@gmail.com>
2019-08-12 19:35:54 +0200

    Initial commit.m

---
+++ workspace/CheapPark/CheapPark.Tests/ParkingMachineTests.cs
@@ -0,0 +1,48 @@
+using Shouldly;
+using Xunit;
+
+namespace CheapPark.Tests
+{
+    public class ParkingMachineTests
+    {
+        readonly ParkingMachine machine = new ParkingMachine();
+
+        [Fact]
+        public void InitializedWithNoBalance()
+        {
+            machine.GetBalance().ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void CheckoutCosts200()
+        {
+            machine.Checkout("AB 12 456");
+
+            var item = machine.GetBalance().ShouldHaveSingleItem();
+            item.Key.ShouldBe("AB 12 456");
+            item.Value.ShouldBe(200);
+        }
+
+        [Fact]
+        public void CheckoutTwiceCosts400()
+        {
+            machine.Checkout("AB 12 456");
+            machine.Checkout("AB 12 456");
+
+            var item = machine.GetBalance().ShouldHaveSingleItem();
+            item.Key.ShouldBe("AB 12 456");
+            item.Value.ShouldBe(400);
+        }
+
+        [Fact]
+        public void TwoDifferentCheckouts()
+        {
+            machine.Checkout("AB 12 456");
+            machine.Checkout("DE 13 337");
+
+            machine.GetBalance().Count.ShouldBe(2);
+            machine.GetBalance()["AB 12 456"].ShouldBe(200);
+            machine.GetBalance()["DE 13 337"].ShouldBe(200);
+        }
+    }
+}
---
+++ workspace/CheapPark/CheapPark/ParkingMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheapPark
+{
+    public class ParkingMachine
+    {
+        readonly List<(string licensePlate, int amount)> transactions = new List<(string, int)>();
+
+        public void Checkout(string licensePlate)
+        {
+            transactions.Add((licensePlate, 200));
+        }
+
+        public IReadOnlyDictionary<string, int> GetBalance()
+        {
+            return transactions
+                .GroupBy(x => x.licensePlate, x => x.amount)
+                .ToDictionary(x => x.Key, x => x.Sum());
+        }
+    }
+}
